d7a48bfb1271dde6ba4896bee25818fcef29644f
Cecilia <earwen1986@gmail.com>
2020-12-26 13:06:08 +0000

    Inital key setup

---
+++ challenge-weather-app/assets/index.js
@@ -0,0 +1,3 @@
+
+let meteoUrl = `http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=${meteoKey}`;
+let photoUrl = `https://api.unsplash.com/search/photos?query=snow&client_id=${photoKey}`;
